719177f275555221710f799c9c083395785bd78a
slap-py <slapstersthegod@gmail.com>
2023-04-15 16:07:06 -0700

    modify mapping and index

--- static/mapping.js
+++ static/mapping.js
@@ -17,9 +17,16 @@ function createDepot_visual(title,coordinates,level,buses){
     depotGroup.addLayer(marker1)
 }
 
-function createStop_visual(title,coordinates,routes){
+function createStop_visual(title,coordinates,routes,amenities){
     marker1 = L.marker (coordinates,{icon:stopIcon})
-    marker1.bindPopup(`<strong>${title}</strong><br>Routes: ${routes.join(', ')}`)
+    amenitiesToIcons = {'br':'🚴','ld':'📺','ada':'♿','sh':'🏠','se':'💺'}
+    icons = []
+    for(i=0;i<amenities.length;i++){
+        amenity = amenities[i]
+        icons.push(amenitiesToIcons[amenity])
+
+    }
+    marker1.bindPopup(`<strong>${title}</strong><br>Routes: ${routes.join(', ')}<br>${icons.join('')}`)
     stopGroup.addLayer(marker1)
 }
 
@@ -37,17 +44,17 @@ createDepot_visual('Eastgate Depot',[47.576852494687834, -122.13284016835827],1,
 createDepot_visual('University District Depot',[47.66450428715962, -122.31910048389594],3,'29/45')
 createDepot_visual('White Center Depot',[47.50670752868295, -122.35001505264952],1,'3/20')
 
-createStop_visual('3rd & Seneca', [47.606931816681566, -122.3351124129687],['14','230'])
-createStop_visual('1st & Stewart', [47.610444151300676, -122.34140160419284],['17','38'])
-createStop_visual('1st & Union', [47.60770607349066, -122.33912025605323],['17','230'])
+createStop_visual('3rd & Seneca', [47.606931816681566, -122.3351124129687],['14','230'],['sh','ada'])
+createStop_visual('1st & Stewart', [47.610444151300676, -122.34140160419284],['17','38'],['sh','ada'])
+createStop_visual('1st & Union', [47.60770607349066, -122.33912025605323],['17','230'],['sh','ada'])
 
-createStop_visual('Pine & Alaskan', [47.60872352035995, -122.3437810113533],['4','B'])
-createStop_visual('23rd & Yesler', [47.60178113259038, -122.30370247299345],['53','29','630'])
-createStop_visual('Weller & Rainier', [47.5976186696487, -122.31271099433737],['17'])
+createStop_visual('Pine & Alaskan', [47.60872352035995, -122.3437810113533],['4','B'],['ada'])
+createStop_visual('23rd & Yesler', [47.60178113259038, -122.30370247299345],['53','29','630'],['ada','br'])
+createStop_visual('Weller & Rainier', [47.5976186696487, -122.31271099433737],['17'],['sh','ada'])
 
-createStop_visual('Westlake & Mercer', [47.62448035843266, -122.33807076006781],['49'])
-createStop_visual('Thomas & Terry', [47.62102033511989, -122.33706372894517],['17','41','48','52'])
-createStop_visual('6th & King', [47.59830258846859, -122.3261657656141],['17','211'])
+createStop_visual('Westlake & Mercer', [47.62448035843266, -122.33807076006781],['49'],['sh','ada'])
+createStop_visual('Thomas & Terry', [47.62102033511989, -122.33706372894517],['17','41','48','52'],['sh','ada'])
+createStop_visual('6th & King', [47.59830258846859, -122.3261657656141],['17','211'],['sh','ada'])
 
 L.circle([47.59830258846859, -122.3261657656141],{radius:100}).addTo(map)
 L.circle([47.5991734625748, -122.32640201444418], {radius: 100}).addTo(map);
